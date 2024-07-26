045c56062532666675d1ea2ef6fc305c7d7f969e
Krystian Potaczek <krystian.potaczek07@gmail.com>
2024-07-26 21:51:20 +0200

    Add POST Api

---
+++ src/main/java/com/songify/SongViewController.java
@@ -0,0 +1,32 @@
+package com.songify;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class SongViewController {
+
+    Map<Integer, String> database = new HashMap<>();
+
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/view/songs")
+    public String songs(Model model) {
+        database.put(1, "shawnmendes song1");
+        database.put(2, "ariana grande song2");
+        database.put(3, "ariana grande song3");
+        database.put(4, "shawnmendes song4");
+
+        model.addAttribute("songMap", database);
+
+        return "songs";
+    }
+}
--- src/main/java/com/songify/SongsController.java
+++ src/main/java/com/songify/SongsController.java
@@ -3,10 +3,7 @@ package com.songify;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,28 +12,42 @@ import java.util.Map;
 @Log4j2
 public class SongsController {
 
-    Map<Integer, String> database = new HashMap<>();
+    Map<Integer, String> database = new HashMap<>(Map.of(
+            1, "shawnmendes song 1",
+            2, "ariana grande song 2",
+            3, "the weekend song 3",
+            4, "imagine dragons song 4"
+    ));
 
     @GetMapping("/songs")
     public ResponseEntity<SongResponseDto> getAllSongs() {
-        database.put(1, "shawnmendes song1");
-        database.put(2, "ariana grande song2");
-        SongResponseDto response = new SongResponseDto(database);
 
+        SongResponseDto response = new SongResponseDto(database);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/songs/{id}")
-    public ResponseEntity<SingleSongResponseDto> getSongById(@PathVariable Integer id, @RequestHeader(required = false) String requestId) {
+    public ResponseEntity<SingleSongResponseDto> getSongById(
+            @PathVariable Integer id,
+            @RequestHeader(required = false) String requestId) {
+
         log.info(requestId);
         String song = database.get(id);
 
         if (song == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-
         }
 
         SingleSongResponseDto response = new SingleSongResponseDto(song);
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/songs")
+    public ResponseEntity<SingleSongResponseDto> postSong(@RequestBody SongRequestDto request) {
+        String songName = request.songName();
+        log.info("adding new song {}", songName);
+        database.put(database.size() + 1, songName);
+
+        return ResponseEntity.ok(new SingleSongResponseDto(songName));
     }
 }
